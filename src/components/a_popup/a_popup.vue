ec4b1a8e22b3fec8500a2d4879387a27aa996782
Igor Kobozev <IKobozev@synegy.ru>
2023-10-01 22:04:59 +0300

    finish modal-window-form

---
+++ src/components/a_popup/a_popup.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import "./a_popup.scss";
+
+defineProps<{ visible: Boolean }>();
+const emit = defineEmits(["close"]);
+
+function closePopup() {
+  emit("close", false);
+}
+</script>
+
+<template>
+  <div class="a-popup" v-if="visible">
+    <div class="a-popup__backdrop" @click="closePopup"></div>
+    <div class="a-popup__container">
+      <div class="a-popup__close">
+        <i class="a-popup__i si-close" @click="closePopup"></i>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
